af9f18b96411b9d3ee5886a38763897720cf568a
freespace <freespace.example.com>
2016-06-09 18:21:45 +0700

    add user.model

---
+++ models/user.model.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+//personal infomation
+	name:String,
+	picture:String,
+	gender:String,
+	phone:String,
+	shirt_size:String,
+	regId:String,
+	facebookData:{},
+	
+	own_channels:[],
+	join_events:[],
+	interest_events:[],
+	subscribe_channels:[],
+
+//stat
+	tag_visit:{},
+	channel_visit:{},
+
+
+	tokenDelete:{
+		type:Boolean,
+		default: false
+	},
+	created_date:{
+		type: Date,
+		default: Date.now
+	},
+	lastModified:{
+		type:Date,
+		default: Date.now
+	},
+	lastOnline:{
+		type:Date,
+		default: Date.now
+	}
+});
