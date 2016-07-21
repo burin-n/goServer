57c2a6d01e7cf67c5ba858c7f9cb0f42fd2a3c3a
freespace <freespace.example.com>
2016-07-21 13:34:39 +0700

    add detail in product env

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,6 @@
 module.exports = {
 	debug : false,
-	mongoUri: 'mongodb://localhost/EVH'
+	mongoUri: (process.env.OPENSHIFT_MONGODB_DB_URL ||'mongodb://localhost/' ) + 'dev',
+	PORT : process.env.OPENSHIFT_NODEJS_PORT || 1111,
+	IP : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 }
