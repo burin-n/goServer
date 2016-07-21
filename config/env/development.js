6bb9ba1043d34fced4ef32db2dc07882456ad3ab
freespace <freespace.example.com>
2016-07-21 12:24:46 +0700

    1111->8080

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
 	debug : true,
 	mongoUri: (process.env.OPENSHIFT_MONGODB_DB_URL ||'mongodb://localhost/' ) + 'dev',
-	PORT : process.env.OPENSHIFT_NODEJS_PORT || 1111,
+	PORT : process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	IP : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 };
