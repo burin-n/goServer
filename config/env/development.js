b115a2326285004c557e85fc169cbbbb83c0c492
freespace <freespace.example.com>
2016-08-09 21:53:46 +0700

    delete openshift

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
 	debug : true,
-	mongoUri: (process.env.OPENSHIFT_MONGODB_DB_URL ||'mongodb://localhost/' ) + 'dev',
-	PORT : process.env.OPENSHIFT_NODEJS_PORT || 1111,
-	IP : process.env.OPENSHIFT_NODEJS_IP || '128.199.207.29'
+	mongoUri: (process.env.MONGODB_DB_URL ||'mongodb://localhost/' ) + 'test',
+	PORT : process.env.PORT || 1111,
+	IP : process.env.IP || 'localhost'
 };
