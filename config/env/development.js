7b3b7f1d2a922f88b91879fd502eeebe619204f1
freespace <freespace.example.com>
2016-08-09 21:12:07 +0700

    change PORT & IP

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
 	debug : true,
 	mongoUri: (process.env.OPENSHIFT_MONGODB_DB_URL ||'mongodb://localhost/' ) + 'dev',
-	PORT : process.env.OPENSHIFT_NODEJS_PORT || 8080,
-	IP : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+	PORT : process.env.OPENSHIFT_NODEJS_PORT || 1111,
+	IP : process.env.OPENSHIFT_NODEJS_IP || '128.199.207.29'
 };
